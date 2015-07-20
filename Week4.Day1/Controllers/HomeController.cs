58470650b0d6972c06b8a3993601647d3a5df26b
Scott Ferguson <esfergus@gmail.com>
2015-07-20 16:25:20 -0500

    Adding student class

--- Week4.Day1/Controllers/HomeController.cs
+++ Week4.Day1/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Week4.Day1.Models;
 
 namespace Week4.Day1.Controllers
 {
@@ -13,10 +14,25 @@ namespace Week4.Day1.Controllers
             return View();
         }
 
-        public ActionResult ListClassMates()
+        [HttpGet]
+        public ActionResult classmate()
         {
             return View();
         }
+
+        public ActionResult ListClassMates()
+        {
+            List<Student> BackendClass = new List<Student>();
+            BackendClass.Add(new Student() { FirstName = "Scott", LastName = "Ferguson", HairColor = "Light Brown", HeightInches = 70});
+            BackendClass.Add(new Student() { FirstName = "Aaron", LastName = "Hudson", HairColor = "Dark Brown", HeightInches = 73 });
+            BackendClass.Add(new Student() { FirstName = "Mike", LastName = "Null", HairColor = "Light Brown", HeightInches = 70});
+            BackendClass.Add(new Student() { FirstName = "David", LastName = "Plate", HairColor = "Brown", HeightInches = 75 });
+            BackendClass.Add(new Student() { FirstName = "Jason", LastName = "Williams", HairColor = "Dark Brown", HeightInches = 70});
+            BackendClass.Add(new Student() { FirstName = "Brandon", LastName = "Goza", HairColor = "Brown", HeightInches = 71});
+
+            return View();
+        }
+
 
         public ActionResult About()
         {
